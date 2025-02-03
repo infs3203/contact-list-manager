9de5e2505d6483bd05a5e82c37d54737362be400
Ibrahim Bennbaya <60103187@udst.edu.qa>
2025-02-03 19:51:32 +0300

    name field character limit added
    the name field now has a character limit of 25 instead of 100 which could break the UI

--- forms.py
+++ forms.py
@@ -1,3 +1,4 @@
+# forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import DataRequired, Length, Regexp, Email
@@ -5,15 +6,15 @@ from wtforms.validators import DataRequired, Length, Regexp, Email
 class ContactForm(FlaskForm):
     name = StringField('Name', validators=[
         DataRequired(message="Name is required."),
-        Length(min=2, max=100, message="Name must be between 2 and 100 characters."),
+        Length(min=2, max=25, message="Name must be between 2 and 25 characters."),
         Regexp(r'^[A-Za-z][A-Za-z0-9]*$', message="Name must start with a letter and can contain only letters and digits.")
-    ])
+    ], render_kw={"maxlength": "25"})
     phone = StringField('Phone', validators=[
         DataRequired(message="Phone number is required."),
         Regexp(r'^\+?[0-9]*$', message="Phone number must contain only digits and an optional '+' symbol.")
     ])
     email = StringField('Email', validators=[
-        Email(message="Please enter a valid email address.")  # Email validation
+        Email(message="Please enter a valid email address.")
     ])
     type = SelectField('Type', choices=[
         ('Personal', 'Personal'), 
@@ -21,5 +22,3 @@ class ContactForm(FlaskForm):
         ('Other', 'Other')
     ])
     submit = SubmitField('Submit')
-
-
