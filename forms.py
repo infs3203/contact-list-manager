14e582412d238a14b2a6a7a6e01a272655c6d9a4
Saba Wynne <sWX1283940@china.huawei.com>
2025-02-04 17:10:47 +0300

    The form will not be submitted if fields are empty

--- forms.py
+++ forms.py
@@ -1,12 +1,23 @@
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, SelectField, SubmitField
+
+# class ContactForm(FlaskForm):
+#     name = StringField('Name')
+#     phone = StringField('Phone')
+#     email = StringField('Email')
+#     type = SelectField('Type', 
+#                       choices=[('Personal', 'Personal'), 
+#                               ('Work', 'Work'), 
+#                               ('Other', 'Other')])
+#     submit = SubmitField('Submit') 
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
+from wtforms.validators import DataRequired, Email, Regexp
 
 class ContactForm(FlaskForm):
-    name = StringField('Name')
-    phone = StringField('Phone')
-    email = StringField('Email')
-    type = SelectField('Type', 
-                      choices=[('Personal', 'Personal'), 
-                              ('Work', 'Work'), 
-                              ('Other', 'Other')])
-    submit = SubmitField('Submit') 
+    name = StringField('Name', validators=[DataRequired()])
+    phone = StringField('Phone', validators=[DataRequired(), Regexp(r'^\d+$', message="Phone number must contain only digits.")])
+    email = StringField('Email', validators=[DataRequired(), Email(message="Invalid email format.")])
+    type = SelectField('Type', choices=[('personal', 'Personal'), ('work', 'Work'), ('other', 'Other')], validators=[DataRequired()])
+    submit = SubmitField('Submit')
