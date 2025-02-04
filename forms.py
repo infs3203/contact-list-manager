cf387f9a464b650e392fde714acd2a3f84f36c2b
Saba Wynne <sWX1283940@china.huawei.com>
2025-02-04 17:21:58 +0300

    The user now cannot enter huge amount of data

--- forms.py
+++ forms.py
@@ -13,11 +13,12 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import DataRequired, Email, Regexp
+from wtforms.validators import DataRequired, Email, Regexp, Length
 
 class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    phone = StringField('Phone', validators=[DataRequired(), Regexp(r'^\d+$', message="Phone number must contain only digits.")])
-    email = StringField('Email', validators=[DataRequired(), Email(message="Invalid email format.")])
+    name = StringField('Name', validators=[DataRequired(), Length(max=100)])  # Max length 100
+    phone = StringField('Phone', validators=[DataRequired(), Regexp(r'^\d+$', message="Phone number must contain only digits."), Length(max=15)])  # Max length 15
+    email = StringField('Email', validators=[DataRequired(), Email(message="Invalid email format."), Length(max=100)])  # Max length 100
     type = SelectField('Type', choices=[('personal', 'Personal'), ('work', 'Work'), ('other', 'Other')], validators=[DataRequired()])
     submit = SubmitField('Submit')
+
